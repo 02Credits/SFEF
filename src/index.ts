ef055d1ef34ae517a61cb51593de268867ab82c4
Keith Simmons <keith@the-simmons.net>
2016-11-12 23:15:53 -0800

    pixi

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+var renderer =
+    PIXI.autoDetectRenderer(
+        window.innerWidth,
+        window.innerHeight,
+        {antialias: false, transparent: false, resolution: 1});
+
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+renderer.render(stage);
